9a2a3abbb1451b5fa20156e526782fc0f29aaee6
JasonP77 <llvslr77@gmail.com>
2020-01-29 17:29:56 -0500

    Finished

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,15 @@
 class Dog
-  attr_reader :owner_name
-  def initialize(dog_name, owner_name)
-    @dog_name = dog_name
-    @owner_name = owner_name
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all = []
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Owner
   attr_reader :name, :species
   @@every = []
@@ -22,6 +23,84 @@ class Owner
   def self.reset_all
     @@every = []
   end
+
+  def cats 
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+  def dogs 
+    Dog.all.select do |dog|
+      dog.owner == self
+  end
 end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    newArr =[]
+    Dog.all.each do |dog|
+      if dog.owner == self
+        newArr << dog
+      end
+    end
+    newArr.map{|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    newArr =[]
+    Cat.all.each do |cat|
+      if cat.owner == self
+        newArr << cat
+      end
+    end
+    newArr.map{|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    ownerCats = []
+    ownerDogs = []
+    Dog.all.each do |dog|
+      if dog.owner == self
+        ownerDogs << dog
+      end
+    end
+    Cat.all.each do |cat|
+      if cat.owner == self
+        ownerCats << cat
+      end
+    end
+    ownerDogs.map{|dog| dog.mood = "nervous"}
+    ownerDogs.map{|dog| dog.owner = nil}
+    ownerCats.map{|cat| cat.mood = "nervous"}
+    ownerCats.map{|cat| cat.owner = nil}
+  end
+
+
+
+
+    def list_pets
+      countDogs = 0
+      countCats = 0
+      Dog.all.each do |dog|
+        if dog.owner == self
+          countDogs += 1
+        end
+      end
+      Cat.all.each do |cat|
+        if cat.owner == self
+          countCats += 1
+        end
+      end
+      return "I have #{countDogs} dog(s), and #{countCats} cat(s)."
+    end
+  end
+
 
 
